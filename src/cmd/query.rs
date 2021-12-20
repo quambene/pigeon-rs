30fd3c1fb4ca121afb47bbfb3c3e51813a83d623
quambene <contact.quambene@gmail.com>
2021-12-20 12:59:25 +0100

    Test query subcommand

--- src/cmd/query.rs
+++ src/cmd/query.rs
@@ -97,3 +97,75 @@ pub fn query(matches: &ArgMatches<'_>) -> Result<(), anyhow::Error> {
         Err(anyhow!("Missing argument '{}'", cmd::QUERY))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{app, cmd};
+    use std::env;
+
+    #[test]
+    #[ignore]
+    fn test_display_query() {
+        let test_query = env::var("TEST_QUERY").expect("Missing environment variable 'TEST_QUERY'");
+        let args = vec![cmd::BIN, cmd::QUERY, test_query.as_str(), "--display"];
+
+        let app = app();
+        let matches = app.get_matches_from(args);
+        let subcommand_matches = matches.subcommand_matches(cmd::QUERY).unwrap();
+        println!("subcommand matches: {:#?}", subcommand_matches);
+
+        let res = query(&subcommand_matches);
+        println!("res: {:#?}", res);
+
+        assert!(res.is_ok())
+    }
+
+    #[test]
+    #[ignore]
+    fn test_save_query() {
+        let test_query = env::var("TEST_QUERY").expect("Missing environment variable 'TEST_QUERY'");
+        let args = vec![
+            cmd::BIN,
+            cmd::QUERY,
+            test_query.as_str(),
+            "--display",
+            "--save",
+        ];
+
+        let app = app();
+        let matches = app.get_matches_from(args);
+        let subcommand_matches = matches.subcommand_matches(cmd::QUERY).unwrap();
+        println!("subcommand matches: {:#?}", subcommand_matches);
+
+        let res = query(&subcommand_matches);
+        println!("res: {:#?}", res);
+
+        assert!(res.is_ok())
+    }
+
+    #[test]
+    #[ignore]
+    fn test_save_dir() {
+        let test_query = env::var("TEST_QUERY").expect("Missing environment variable 'TEST_QUERY'");
+        let args = vec![
+            cmd::BIN,
+            cmd::QUERY,
+            test_query.as_str(),
+            "--display",
+            "--save",
+            "--save-dir",
+            "./my-saved-queries",
+        ];
+
+        let app = app();
+        let matches = app.get_matches_from(args);
+        let subcommand_matches = matches.subcommand_matches(cmd::QUERY).unwrap();
+        println!("subcommand matches: {:#?}", subcommand_matches);
+
+        let res = query(&subcommand_matches);
+        println!("res: {:#?}", res);
+
+        assert!(res.is_ok())
+    }
+}
