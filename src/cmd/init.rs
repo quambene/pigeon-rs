63a9a7299508ee48e59bf2a9a81cd90915d84dff
quambene <contact.quambene@gmail.com>
2024-03-30 17:12:52 +0100

    Test init

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -1,10 +1,53 @@
 use crate::{arg, email_builder::MessageTemplate};
+use anyhow::Context;
 use clap::ArgMatches;
+use std::{env, io};
 
 pub fn init(matches: &ArgMatches) -> Result<(), anyhow::Error> {
     if matches.is_present(arg::VERBOSE) {
         println!("matches: {:#?}", matches);
     }
 
-    MessageTemplate::create(matches)
+    let current_dir = env::current_dir().context("Can't get current directory")?;
+    let file_name = MessageTemplate::file_name();
+    let template_path = current_dir.join(file_name);
+
+    match template_path.exists() {
+        true => {
+            let mut input = String::new();
+            println!("Message template already exists. Should this template be overwritten? Yes (y) or no (n)");
+            loop {
+                io::stdin()
+                    .read_line(&mut input)
+                    .context("Can't read input")?;
+                match input.trim() {
+                    "y" | "yes" | "Yes" => {
+                        println!(
+                            "Overwriting message template in current directory '{}' ...",
+                            template_path.display()
+                        );
+                        MessageTemplate::write(&template_path)?;
+                        break;
+                    }
+                    "n" | "no" | "No" => {
+                        println!("Aborted ...");
+                        break;
+                    }
+                    _ => {
+                        println!("Choose yes (y) or no (n). Try again.");
+                        continue;
+                    }
+                }
+            }
+        }
+        false => {
+            println!(
+                "Creating message template in current directory: {} ...",
+                template_path.display()
+            );
+            MessageTemplate::write(&template_path)?;
+        }
+    }
+
+    Ok(())
 }
--- src/email_builder/message_template.rs
+++ src/email_builder/message_template.rs
@@ -3,12 +3,13 @@ use anyhow::{anyhow, Context, Result};
 use clap::ArgMatches;
 use serde::Deserialize;
 use std::{
-    env, fs,
-    io::{self, Write},
-    path::PathBuf,
+    fs,
+    io::Write,
+    path::{Path, PathBuf},
 };
 
 const TEMPLATE_FILE_NAME: &str = "message.yaml";
+
 static MESSAGE_TEMPLATE: &str = r##"# Specify the subject, plaintext and html version of your email.
 # Personalize message by wrapping variables in curly brackets, eg. {first_name}.
 
@@ -28,51 +29,8 @@ pub struct MessageTemplate {
 }
 
 impl MessageTemplate {
-    pub fn create(_matches: &ArgMatches) -> Result<(), anyhow::Error> {
-        let current_dir = env::current_dir().context("Can't get current directory")?;
-        let path_dir = current_dir;
-
-        let file_name = TEMPLATE_FILE_NAME;
-        let template_path = path_dir.join(file_name);
-
-        match template_path.exists() {
-            true => {
-                let mut input = String::new();
-                println!("Message template already exists. Should this template be overwritten? Yes (y) or no (n)");
-                loop {
-                    io::stdin()
-                        .read_line(&mut input)
-                        .context("Can't read input")?;
-                    match input.trim() {
-                        "y" | "yes" | "Yes" => {
-                            println!(
-                                "Overwriting message template in current directory '{:#?}' ...",
-                                template_path
-                            );
-                            create_template(template_path)?;
-                            break;
-                        }
-                        "n" | "no" | "No" => {
-                            println!("Aborted ...");
-                            break;
-                        }
-                        _ => {
-                            println!("Choose yes (y) or no (n). Try again.");
-                            continue;
-                        }
-                    }
-                }
-            }
-            false => {
-                println!(
-                    "Creating message template in current directory: {:#?} ...",
-                    template_path
-                );
-                create_template(template_path)?;
-            }
-        }
-
-        Ok(())
+    pub fn file_name() -> &'static str {
+        TEMPLATE_FILE_NAME
     }
 
     pub fn read(matches: &ArgMatches) -> Result<Self, anyhow::Error> {
@@ -99,11 +57,11 @@ impl MessageTemplate {
             Err(anyhow!("Missing argument '{}'", arg::MESSAGE_FILE))
         }
     }
-}
 
-fn create_template(path: PathBuf) -> Result<(), anyhow::Error> {
-    let mut message_template =
-        fs::File::create(path).context("Unable to create message template.")?;
-    message_template.write_all(MESSAGE_TEMPLATE.as_bytes())?;
-    Ok(())
+    pub fn write(path: &Path) -> Result<(), anyhow::Error> {
+        let mut message_template =
+            fs::File::create(path).context("Unable to create message template.")?;
+        message_template.write_all(MESSAGE_TEMPLATE.as_bytes())?;
+        Ok(())
+    }
 }
