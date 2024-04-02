65ed68b73cfb71b2ffb0f0aa66ad4ae44bebb846
quambene <contact.quambene@gmail.com>
2024-04-03 01:42:53 +0200

    Refactor query test

--- tests/cmd/test_query.rs
+++ tests/cmd/test_query.rs
@@ -4,63 +4,36 @@
      - DB_USER
      - DB_PASSWORD
      - DB_NAME
-     - TEST_QUERY
 */
 
 use assert_cmd::Command;
 use predicates::str;
-use std::{env, fs};
+use std::fs;
 use tempfile::tempdir;
 
 #[test]
-#[ignore]
 fn test_query_display() {
-    let test_query = env::var("TEST_QUERY").expect("Missing environment variable 'TEST_QUERY'");
+    let test_query = "select email, first_name, last_name from account";
+
     println!("Execute 'pigeon query {test_query} --display'");
     let mut cmd = Command::cargo_bin("pigeon").unwrap();
-    cmd.args(["query", test_query.as_str(), "--display"]);
-    cmd.assert()
-        .success()
-        .stdout(str::contains("Display query result"));
+    cmd.args(["query", test_query, "--display"]);
+    cmd.assert().success().stdout(str::contains(
+        "Display query result: shape: (2, 3)
+┌────────────────────────────┬────────────┬──────────────┐
+│ email                      ┆ first_name ┆ last_name    │
+│ ---                        ┆ ---        ┆ ---          │
+│ str                        ┆ str        ┆ str          │
+╞════════════════════════════╪════════════╪══════════════╡
+│ marie@curie.com            ┆ Marie      ┆ Curie        │
+│ alexandre@grothendieck.com ┆ Alexandre  ┆ Grothendieck │
+└────────────────────────────┴────────────┴──────────────┘",
+    ));
 }
 
 #[test]
-#[ignore]
 fn test_query_save() {
-    let test_query = env::var("TEST_QUERY").expect("Missing environment variable 'TEST_QUERY'");
-    let temp_dir = tempdir().unwrap();
-    let temp_path = temp_dir.path();
-    assert!(temp_path.exists(), "Missing path: {}", temp_path.display());
-    let save_dir = temp_path.to_str().unwrap();
-
-    println!("Execute 'pigeon query {test_query} --save'");
-    let mut cmd = Command::cargo_bin("pigeon").unwrap();
-    cmd.current_dir(save_dir);
-    cmd.args(["query", test_query.as_str(), "--save"]);
-    cmd.assert()
-        .success()
-        .stdout(str::contains("Save query result to file"));
-
-    if let Ok(mut entries) = fs::read_dir(temp_path.join("saved_queries")) {
-        let dir_entry = entries.find_map(|entry| {
-            if let Ok(entry) = entry {
-                if entry.file_name().to_str().is_some_and(|file_name| {
-                    file_name.contains("query") && file_name.ends_with(".csv")
-                }) {
-                    return Some(entry);
-                }
-            }
-
-            None
-        });
-        assert!(dir_entry.is_some());
-    }
-}
-
-#[test]
-#[ignore]
-fn test_query_save_dir() {
-    let test_query = env::var("TEST_QUERY").expect("Missing environment variable 'TEST_QUERY'");
+    let test_query = "select email, first_name, last_name from account";
     let temp_dir = tempdir().unwrap();
     let temp_path = temp_dir.path();
     assert!(temp_path.exists(), "Missing path: {}", temp_path.display());
@@ -68,16 +41,10 @@ fn test_query_save_dir() {
 
     println!("Execute 'pigeon query {test_query} --save --save-dir {save_dir}'");
     let mut cmd = Command::cargo_bin("pigeon").unwrap();
-    cmd.args([
-        "query",
-        test_query.as_str(),
-        "--save",
-        "--save-dir",
-        save_dir,
-    ]);
+    cmd.args(["query", test_query, "--save", "--save-dir", save_dir]);
     cmd.assert()
         .success()
-        .stdout(str::contains("Save query result to file"));
+        .stdout(str::contains("Save query result to file:"));
 
     if let Ok(mut entries) = fs::read_dir(temp_path) {
         let dir_entry = entries.find_map(|entry| {
