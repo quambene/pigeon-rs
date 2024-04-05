b75c60a29740554d54c0114bfc7f6bd13dae4d21
quambene <contact.quambene@gmail.com>
2024-04-05 02:11:32 +0200

    Add test table

--- test_data/init.sql
+++ test_data/init.sql
@@ -5,6 +5,10 @@ CREATE TABLE account (
     email character varying NOT NULL
 );
 
+CREATE TABLE images (id serial PRIMARY KEY, image bytea);
+
 COPY account(first_name, last_name, email)
 FROM
     '/docker-entrypoint-initdb.d/contacts.csv' DELIMITER ',' CSV HEADER;
+
+INSERT INTO images (image) VALUES (pg_read_binary_file('/docker-entrypoint-initdb.d/test.png'));
